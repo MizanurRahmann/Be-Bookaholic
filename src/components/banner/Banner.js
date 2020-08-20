db2ca2145aa7afefad36eb49513485627cf8499f
MizanurRahmann <mr926560@gmail.com>
2020-08-20 17:21:22 +0600

    add: banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import bannerImage from '../../styles/images/bannerImage/human1.png';
+import '../../styles/css/Banner.css';
 
 function Banner() {
     return (
-        <div style={{marginTop: "80px"}}>
-            Banner
+        <div className="banner">
+            <div className="banner__text">
+                <div className="banner__heading">
+                    <h1>Be <span>Bookaholic,</span></h1>
+                    <h2>Be Awsome</h2>
+                </div>
+            </div>
+            <div className="banner__image">
+                <img src={bannerImage} alt="humaaans" />
+            </div>
         </div>
     )
 }
