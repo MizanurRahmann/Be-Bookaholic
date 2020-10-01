8b1abfde87dbf1b7462c9a67eafa67dacb4b098d
MizanurRahmann <mr926560@gmail.com>
2020-10-01 21:27:37 +0600

    CHANGE: loading style

--- src/components/books/BookLoading.js
+++ src/components/books/BookLoading.js
@@ -4,12 +4,7 @@ function BookLoading() {
     
     return (
         <div className="loading">
-            <div className="loading-box">
-                <div className="lb lb1"></div>
-                <div className="lb lb2"></div>
-                <div className="lb lb3"></div>
-                <div className="lb lb4"></div>
-            </div>
+            <div className="pulse"></div>
         </div>
     )
 }
