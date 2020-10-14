36c1a042a55f4cbbc22001246694bb2a94c4ae4a
MizanurRahmann <mr926560@gmail.com>
2020-10-14 07:46:09 +0600

    FIX: card item length issue.

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -42,7 +42,11 @@ const Nav = props => {
 
                     <Link className="logo cart" to='/checkout' onClick={props.backDropHandler}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" strokeLineJoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
-                        <div>{state.basket.length}</div>
+                        {
+                            state.basket.length > 0
+                            ? <div>{state.basket.length}</div>
+                            : null
+                        }
                     </Link>
 
                     <div className="logo books" onClick={props.toggleBookListOption}>
