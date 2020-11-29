b3ad5d9ead7887d45dea99e2e576c7797d01673c
MizanurRahmann <mr926560@gmail.com>
2020-11-29 20:29:16 +0600

    ADD: react form progress.

--- src/components/order/Shopping.js
+++ src/components/order/Shopping.js
@@ -1,8 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import '../../styles/css/shopping.css';
-import { useStateValue } from '../Context/StateProvider';
-import clipboard from '../../styles/images/checkout/clipboard.png';
-import PIForm from './PIForm';
+import React, { useState, useEffect, Component } from "react";
+import Stepper from "react-stepper-horizontal";
+import { useStateValue } from "../Context/StateProvider";
+
+//Assets
+import clipboard from "../../styles/images/checkout/clipboard.png";
+import "../../styles/css/shopping.css";
+
+// Components
+import PIForm from "./PIForm";
 
 function Shopping() {
     const [count, setCount] = useState(0);
@@ -12,32 +17,54 @@ function Shopping() {
         <div className="shopping">
             {/* SHOPPING TASKS */}
             <div className="shopping__tasks">
-                <div className="shopping__tasks-progress"></div>
+                <div className="shopping__tasks-progress">
+                    <Stepper
+                        className="stepperjs"
+                        steps={[
+                            { title: "Shipping Address" },
+                            { title: "Payment" },
+                            { title: "Confirm Order" },
+                        ]}
+                        activeStep={1}
+                        size={40}
+                        activeColor="#3465BF"
+                        completeColor="#2F3973"
+                        circleFontSize={20}
+                    />
+                </div>
                 <div className="task">
-
                     <div className="shopping__tasks-task">
-                        <PIForm count={count} setCount={setCount}/>
+                        <PIForm count={count} setCount={setCount} />
                     </div>
                 </div>
             </div>
+
+            {/* SHOPPING CALCULATIONS */}
             <div className="checkout__calc">
                 <h1>Order Summary</h1>
                 <div className="checkout__add">
                     <img src={clipboard} alt="delivery-truck" />
                     <div className="checkout__add-text">
-                        <h3>to get <span>15%</span> offer</h3>
-                        <p>buy books more than <span>৳1000</span></p>
+                        <h3>
+                            to get <span>15%</span> offer
+                        </h3>
+                        <p>
+                            buy books more than <span>৳1000</span>
+                        </p>
                     </div>
                 </div>
                 <div className="calc">
                     <div className="calc__item">
-                        <div className="item__name">Number of Books</div> <div>{state.basket.length}</div>
+                        <div className="item__name">Number of Books</div>{" "}
+                        <div>{state.basket.length}</div>
                     </div>
                     <div className="calc__item">
-                        <div className="item__name">Subtotal</div> <div>৳ {state.total}</div>
+                        <div className="item__name">Subtotal</div>{" "}
+                        <div>৳ {state.total}</div>
                     </div>
                     <div className="calc__item">
-                        <div className="item__name">Shipping</div> <div>৳ 50</div>
+                        <div className="item__name">Shipping</div>{" "}
+                        <div>৳ 50</div>
                     </div>
                     <div className="calc__item">
                         <div className="item__name">Total</div>
@@ -48,7 +75,7 @@ function Shopping() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Shopping
+export default Shopping;
