a77c2d0d253b865127c369adbb922350eaebc317
MizanurRahmann <mr926560@gmail.com>
2020-12-02 22:26:37 +0600

    ADD: stripe stuffs to frontend.

---
+++ src/components/order/MasterCard.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useStateValue } from "../Context/StateProvider";
+import axios from "./axios";
+
+function MasterCard({ setCount }) {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const [state, dispatch] = useStateValue();
+
+    const [error, setError] = useState("");
+    const [disable, setDisable] = useState(null);
+    const [processing, setProcessing] = useState(false);
+    const [seccess, setSucces] = useState(false);
+    const [clientSecret, setClientSecret] = useState(true);
+
+    useEffect(() => {
+        // get the special stripe secret which helps us to charge a customer
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: "post",
+                url: `payments/create?total=${state.total * 100}`,
+            });
+            setClientSecret(response.data.clientSecret);
+        };
+
+        getClientSecret();
+    }, [state.basket]);
+
+    // Runs when client confirm for his/her order
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setProcessing(true);
+
+        const payload = await stripe
+            .confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: elements.getElement(CardElement),
+                },
+            })
+            .then(({ paymentIntent }) => {
+                setSucces(true);
+                setError("");
+                setProcessing(false);
+                setCount(2);
+            });
+    };
+
+    // Any change in card element
+    const handleChange = (event) => {
+        setDisable(event.empty);
+        setError(event.error ? event.error.message : "");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement onChange={handleChange} />
+            {error && <div>{error}</div>}
+            <div className="buttonBox">
+                <button className="back" onClick={() => setCount(0)}>
+                    <i class="fas fa-arrow-left"></i> Back
+                </button>
+                <button className="submit" onClick={() => handleSubmit}>
+                    Next Step
+                </button>
+            </div>
+        </form>
+    );
+}
+
+export default MasterCard;
--- src/components/order/PaymentForm.js
+++ src/components/order/PaymentForm.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 // Components
 import CashOnDeliveryMethod from "./CashOnDeliveryMethod";
+import MasterCard from "./MasterCard";
 import SelectPaymentMethod from "./SelectPaymentMethod";
+
+// Stripe publish key
+const promise = loadStripe(
+    "pk_test_51HtcNcEQ7fnc9zNapuT3aeJ5k6HqFmGRZFfzqwUYrloTt1hsoxP90KTrceCQdF2N5PO7Vwl1mr2J6IPY0KZdaq8800nEx2Q9Wr"
+);
 
 function PaymentForm({ count, setCount }) {
     const [method, setMethod] = useState(0);
@@ -12,7 +20,9 @@ function PaymentForm({ count, setCount }) {
             <h1>Payment</h1>
             <SelectPaymentMethod method={method} setMethod={setMethod} />
             {method === 0 ? (
-                <p>MasterCard Method</p>
+                <Elements stripe={promise}>
+                    <MasterCard setCount={setCount} />
+                </Elements>
             ) : method === 1 ? (
                 <p>bKash Method</p>
             ) : method === 2 ? (
